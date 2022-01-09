779def4ef80ec8d81cb96f20379761332cfdb7ec
bùi hưng <hungprovip0089@gmail.com>
2022-01-09 23:11:52 +0700

    random 10 questions for each load

---
+++ script.js
@@ -0,0 +1,19 @@
+$('document').ready(function(){
+    let arr = Array.from({length : 40}, (_, v) => v + 1)
+
+    const shuffleArray = arr => {
+        for (let i = arr.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          const temp = arr[i];
+          arr[i] = arr[j];
+          arr[j] = temp;
+        }
+    }
+
+    shuffleArray(arr);
+
+    arr.slice(0,30).forEach(function(val,idx){
+        var id = "#q" + val.toString();
+        $(id).remove(); 
+    });
+})
