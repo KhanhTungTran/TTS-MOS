fd1cebb5d677a57efc9037c17574e5d7b52bc40f
bùi hưng <hungprovip0089@gmail.com>
2022-01-09 21:42:12 +0700

    fix name

--- render_mos.py
+++ render_mos.py
@@ -16,9 +16,9 @@ def main():
         form_url="https://script.google.com/macros/s/AKfycbzApm3cSoTRMbhTaEgd3c3VtpV9nRP1DUqxXQLsyVz9uAtTrSty/exec",
         form_id=1,
         questions=[{
-                    "title" : "Question " + str(i+1),
-                    "audio_paths": ["wavs/test1.wav","wavs/test2.wav","wavs/test3.wav","wavs/test4.wav"],
-                    "name": "q" + str(i+1)} for i in range(NUM)]
+                    "title" : "Question " + str(i),
+                    "audio_paths": ["wavs/q"+str(i)+"/test1.wav","wavs/q"+str(i)+"/test2.wav","wavs/q"+str(i)+"/test3.wav","wavs/q"+str(i)+"/test4.wav"],
+                    "name": "q" + str(i)} for i in range(1,NUM+1)]
     )
     with open("rendered_mos.html", "w", encoding="utf-8") as f:
         f.write(html)
